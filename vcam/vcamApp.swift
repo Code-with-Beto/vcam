341c3610904ba6d831d8975843dd4033b8bc6ceb
Beto <betomoedano@outlook.com>
2026-09-08 12:49:26 -0500

    Initial Commit

---
+++ vcam/vcamApp.swift
@@ -0,0 +1,17 @@
+//
+//  vcamApp.swift
+//  vcam
+//
+//  Created by Beto on 9/8/26.
+//
+
+import SwiftUI
+
+@main
+struct vcamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
